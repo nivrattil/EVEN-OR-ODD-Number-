980ead8e820a7e3e08d7691a9dd8896a0d43f348
Nivratti Waghmode <94159696+nivrattil@users.noreply.github.com>
2023-02-27 01:19:10 +0530

    Add files via upload

---
+++ EVEN OR ODD Cpp program.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+int num;
+cout << "Enter number: ";
+cin >> num;
+if(num%2==0){
+cout<< "Number is EVEN"; }
+else
+{
+cout<<" Nymber is ODD";
+}
+return 0;
+}
+
